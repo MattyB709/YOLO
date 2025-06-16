019ce00d42eab9b0e3dc366c10bd1d994c5bf550
mattyb709 <matthew.builes8@gmail.com>
2025-06-16 16:29:35 -0500

    updated onnx logic

--- onnx_export.py
+++ onnx_export.py
@@ -1,16 +1,18 @@
 import torch  
 from pathlib import Path  
 from yolo.model.yolo import create_model  
+from yolo.tools.solver import InferenceModel
 from yolo.config.config import Config  
 from hydra import compose, initialize  
 from torch.onnx import export  
   
 # Load configuration  
 with initialize(config_path='yolo/config', version_base=None):
-    cfg = compose(config_name='config', overrides=['model=v9-c'])
+    cfg = compose(config_name='config', overrides=['model=v9-c', 'dataset=ppe_500'])
   
 # Create and load model  
-model = create_model(cfg.model, class_num=cfg.dataset.class_num, weight_path='epoch_0.ckpt')  
+# model = create_model(cfg.model, class_num=4, weight_path='epoch_0.ckpt')  
+model= InferenceModel.load_from_checkpoint("epoch_0.ckpt", cfg) 
 model.eval()  
   
 # Create dummy input matching your image size  
